93335f60e1115e51e8c59236d8b9033819c1e55f
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-03-27 09:28:44 +0600

    Text to voice audio converter.

--- src/main/java/com/spring/ai/chat/service/AudioService.java
+++ src/main/java/com/spring/ai/chat/service/AudioService.java
@@ -2,20 +2,35 @@ package com.spring.ai.chat.service;
 
 
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 
 @Service
 public class AudioService {
 
     private OpenAiAudioTranscriptionModel  openAiAudioTranscriptionModel;
+    private OpenAiAudioSpeechModel audioSpeechModel;
 
-    public AudioService(OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel) {
+    public AudioService(OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel,OpenAiAudioSpeechModel audioSpeechModel) {
         this.openAiAudioTranscriptionModel = openAiAudioTranscriptionModel;
+        this.audioSpeechModel = audioSpeechModel;
     }
 
     public String convertVoiceToText(MultipartFile file, String language) {
@@ -33,5 +48,27 @@ public class AudioService {
         AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(file.getResource(), openAiAudioTranscriptionOptions);
 
         return openAiAudioTranscriptionModel.call(prompt).getResult().getOutput();
+    }
+
+
+    public Resource convertTextToSpace(String text) {
+        OpenAiAudioSpeechOptions options= OpenAiAudioSpeechOptions
+                .builder()
+                .model(OpenAiAudioApi.TtsModel.TTS_1.getValue())
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .voice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+                .speed(0.75)
+                .build();
+
+        TextToSpeechPrompt prompt = new TextToSpeechPrompt(text, options);
+
+        TextToSpeechResponse response = audioSpeechModel.call(prompt);
+        byte[] output = response.getResult().getOutput();
+
+        ByteArrayResource resource = new ByteArrayResource(output);
+
+       Resource file = resource;
+       return file;
+
     }
 }
--- src/main/java/com/spring/ai/controller/AudioController.java
+++ src/main/java/com/spring/ai/controller/AudioController.java
@@ -3,13 +3,15 @@ package com.spring.ai.controller;
 
 import com.spring.ai.chat.service.AudioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/audio")
@@ -24,5 +26,16 @@ public class AudioController {
                                                      @RequestParam(value = "language", required = false) String language) {
         String response = this.audioService.convertVoiceToText(file, language);
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/convert-text-to-voice")
+    public ResponseEntity<Resource> convertTextToAudio(@RequestParam("prompt") String prompt) throws IOException {
+        Resource response = this.audioService.convertTextToSpace(prompt);
+        return ResponseEntity.ok().
+                contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .contentLength(response.contentLength())
+                .header(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename("ai-voice.mp3").build().toString())
+                .body(response);
     }
 }
