f274b208fee234b8d0668ab63e5350ba39185d11
mdmehedi31 <mdmehedihasan.mh31@gmail.com>
2026-03-28 20:25:29 +0600

    pgvector config

--- src/main/java/com/spring/ai/config/VectorLoader.java
+++ src/main/java/com/spring/ai/config/VectorLoader.java
@@ -1,25 +1,27 @@
 package com.spring.ai.config;
 
 
-import org.apache.pdfbox.multipdf.PDFCloneUtility;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
-import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
 import java.io.File;
 import java.util.List;
 
-@Configuration
+/*
+Now we use pgvector so we don't need this class,
+please follow the commit history for getting the flow
+@Configuration*/
+
+
 public class VectorLoader {
 
     @Value("classpath:/bdconstitutions.pdf")
